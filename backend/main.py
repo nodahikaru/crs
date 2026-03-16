2f7719ee120ec42cf9274c096a05d926464bb895
nodahikaru <like365.hondai@gmail.com>
2026-03-16 13:43:22 +0900

    44

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,6 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Request, Response
-from mangum import Mangum
 from routes import router
 
 app = FastAPI(title="Concise IR Report System", version="0.1.0")
@@ -21,5 +19,3 @@ def root():
     return {"message": "Hello World"}
 
 app.include_router(router)
-
-handler = Mangum(app)
